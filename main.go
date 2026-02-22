2e7fb845fd2c8caa5f7824fba853fbfad5e6d8d1
cullenwerks <cullen.guimond@gmail.com>
2026-02-22 01:24:30 -0500

    fix: detect extreme-worker arg in main() to run cobra instead of GUI

--- main.go
+++ main.go
@@ -3,10 +3,18 @@
 package main
 
 import (
+	"os"
+	"syscleaner/cmd"
 	"syscleaner/gui"
 )
 
 func main() {
-	// Always launch GUI - this is a GUI-only application
+	// If invoked as a background worker subprocess, run cobra instead of the GUI.
+	for _, arg := range os.Args[1:] {
+		if arg == "extreme-worker" {
+			cmd.Execute()
+			return
+		}
+	}
 	gui.Run()
 }
